ad391a2eea382f44a27e39f259870010f80d1186
Eric King <e.king117@gmail.com>
2019-12-08 17:35:45 -0500

    added counter button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Button(props) {
+  return (
+    <button onClick={() => {props.clickHandler()}} id="button-inc">+1</button>
+  )
+}
+
+function Display(props) {
+  return (
+    <div>{props.message}</div>
+  )
+}
+
+function App(props) {
+  let [counter, setCounter] = useState(0)
+  const handleClick = () => setCounter(++counter)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button clickHandler={handleClick}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Display message={counter}/> Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
